89dd5fd5d5ca67646bc2ea7ca8dbf6dbb16f6a4f
Udit Kapahi <ukitkk@gmail.com>
2016-08-31 15:30:15 +0530

    CRUD options for Pic

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,7 +1,12 @@
 class PicsController < ApplicationController
 
-  def index
+  before_action :find_pic,only: [:show,:edit,:update,:destroy]
 
+  def index
+    @pics=Pic.all.order("created_at DESC")
+  end
+
+  def show
   end
 
   def new
@@ -10,11 +15,38 @@ class PicsController < ApplicationController
 
   def create
     @pic=Pic.new(picParams)
+    if @pic.save
+      redirect_to pic_path(@pic),notice: "Yess! It was posted."
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+
+    if @pic.update(picParams)
+      redirect_to pic_path(@pic),notice: "Congrats! your pic has been updated."
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @pic.destroy
+    redirect_to root_path
+
   end
 
   private
   def picParams
     params.require(:pic).permit(:title,:description)
+  end
+
+  def find_pic
+    @pic =Pic.find(params[:id])
   end
 
 end
