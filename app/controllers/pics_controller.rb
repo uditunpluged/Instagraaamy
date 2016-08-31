7969835fb357eab7af2cd2a97c822297e2ecc1d9
Udit Kapahi <ukitkk@gmail.com>
2016-08-31 14:43:17 +0530

    created pic action and views

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -4,4 +4,17 @@ class PicsController < ApplicationController
 
   end
 
+  def new
+    @pic=Pic.new
+  end
+
+  def create
+    @pic=Pic.new(picParams)
+  end
+
+  private
+  def picParams
+    params.require(:pic).permit(:title,:description)
+  end
+
 end
